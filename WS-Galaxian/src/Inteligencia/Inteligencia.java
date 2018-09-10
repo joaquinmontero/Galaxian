49ebaf5388aad50f105aa39c53e153de9e1188e1
Yasmin <ymnlopez@gmail.com>
2018-09-10 15:36:45 -0300

    no message

--- WS-Galaxian/src/Inteligencia/Inteligencia.java
+++ WS-Galaxian/src/Inteligencia/Inteligencia.java
@@ -5,10 +5,9 @@ import Logica.*;
  * @author Araceli Iglesias, Sol Stiep, Yasmin Pie Lopez
  *
  */
-public abstract class Inteligencia extends Entidad{
-	public Inteligencia(int velocidad, int x, int y,int vida) {
+public abstract class Inteligencia{
 
-        super(velocidad,x,y,vida);
     }
 	
-}
+
+
